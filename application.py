178f00535534377fd590cf0d643af58cf2a01ba5
Eddie Nguyen <eddienguyen@Eddies-MBP.attlocal.net>
2022-08-02 11:26:29 -0700

    Basic HTTP Error handling

--- application.py
+++ application.py
@@ -11,24 +11,35 @@ from requests.exceptions import Timeout
 
 # Token - DELETE: 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
 
+global today_date
+today_date = date.today().strftime("%m-%d-%y")
+
 
 
 def update_url():
-    today_date = date.today().strftime("%m-%d-%y")
+
     url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/{date}.csv".format(date = today_date)
+    return url
 
     
 
 def get_request():
-    update_url()
-    
-
-    today_date = date.today().strftime("%m-%d-%y")
     try:
         
+        #https://google.com
+    
         request = requests.get(update_url(), timeout=10)
-        open("%s.csv" % today_date, "wb").write(request.content)
+        
+        try:
+            req_status = request.raise_for_status()
+            open("%s.csv" % today_date, "wb").write(request.content)
 
+        except requests.exceptions.HTTPError:
+
+            print(requests.exceptions.HTTPError)
+            print('URL Error')
+            
+    
     except Timeout:
         print("Timeout raised")
 
--- testing.py
+++ testing.py
@@ -1,13 +1,39 @@
+from abc import update_abstractmethods
+from urllib.error import HTTPError
+import requests
+
+from requests import Timeout
 from datetime import date
 
+global today_date
+
+today_date = date.today().strftime("%m-%d-%y")
+
+
 def update_url():
-    today_date = date.today().strftime("%m-%d-%y")
+
     url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports_us/{date}.csv".format(date = today_date)
 
     return url
 
-def print_url():
 
-    print(update_url())
+def get_request():
 
-print_url()
+    try:
+        
+        #https://google.com
+    
+        request = requests.get(update_url(), timeout=10)
+        
+        try:
+            req_status = request.raise_for_status()
+            print('Success')
+
+        except requests.exceptions.HTTPError:
+
+            print(requests.exceptions.HTTPError)
+    
+    except Timeout:
+        print("Timeout raised")
+
+get_request()
