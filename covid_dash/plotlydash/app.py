76189dc31a2de5b15cbc0b7bc17c8050b5a71a3f
Eddie Nguyen <eddienguyen@Eddies-MBP.attlocal.net>
2022-08-03 10:43:35 -0700

    Config.py somewhat done, and gitignore

---
+++ covid_dash/plotlydash/app.py
@@ -0,0 +1,24 @@
+import dash
+
+from dash import dash_table
+from dash import dcc 
+from dash import html
+from dash import Dash
+
+from .data import create_dataframe
+from .layout import html_layout
+
+def init_dashboard(server):
+    dash_app = dash.Dash(
+        server=server, 
+        routes_pathname_prefix='/dashapp/', 
+        external_stylesheets=[
+            '/static/dist/css/styles.css'
+        ]
+    )
+
+    dash_app.layout = html.Div(id = 'dash-container')
+
+    return dash_app.server
+
+
